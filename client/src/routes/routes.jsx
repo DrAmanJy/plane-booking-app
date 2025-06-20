4c44982fba02e2ceab3c76a629dd62aa901bde82
Aman Lathar <dramanjy0770@gmail.com>
2025-06-20 12:06:32 +0530

    updated routes

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,6 +1,19 @@
 import { createBrowserRouter } from "react-router";
+import Home from "../pages/Home";
+import Auth from "../pages/Auth";
+import Flight from "../pages/Flight";
 
-export const routes = createBrowserRouter({
-  path: "",
-  element: <None />,
-});
+export const routes = createBrowserRouter([
+  {
+    path: "",
+    element: <Home />,
+  },
+  {
+    path: "auth",
+    element: <Auth />,
+  },
+  {
+    path: "flight/:flightName",
+    element: <Flight />,
+  },
+]);
