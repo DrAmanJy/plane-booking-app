c251c0a97d5d558c9181cef452cac73f40da6920
Aman Lathar <dramanjy0770@gmail.com>
2025-06-21 15:14:23 +0530

    added new routes

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,19 +1,47 @@
 import { createBrowserRouter } from "react-router";
+import NavLayout from "./NavLayout";
 import Home from "../pages/Home";
 import Auth from "../pages/Auth";
 import Flight from "../pages/Flight";
+import AdminAddFlight from "../pages/AdminAddFlight";
+import MyTickets from "../pages/MyTickets";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 export const routes = createBrowserRouter([
   {
     path: "",
-    element: <Home />,
-  },
-  {
-    path: "auth",
-    element: <Auth />,
-  },
-  {
-    path: "flight/:flightName",
-    element: <Flight />,
+    element: <NavLayout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "auth",
+        element: <Auth />,
+      },
+      {
+        path: "flight/:flightId",
+        element: <Flight />,
+      },
+      {
+        element: <ProtectedRoute allowedRoles={["admin"]} />,
+        children: [
+          {
+            path: "/admin",
+            element: <AdminAddFlight />,
+          },
+        ],
+      },
+      {
+        element: <ProtectedRoute allowedRoles={["user"]} />,
+        children: [
+          {
+            path: "tickets",
+            element: <MyTickets />,
+          },
+        ],
+      },
+    ],
   },
 ]);
